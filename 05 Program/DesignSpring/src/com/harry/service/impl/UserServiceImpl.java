8587a559cb3f1d47fbbb29fbf2d38a5ef21009e7
harryZou <963099010@qq.com>
2016-01-22 17:23:02 +0800

    Add log4j.

--- 05 Program/DesignSpring/src/com/harry/service/impl/UserServiceImpl.java
+++ 05 Program/DesignSpring/src/com/harry/service/impl/UserServiceImpl.java
@@ -2,6 +2,8 @@ package com.harry.service.impl;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import com.harry.common.Const;
 import com.harry.dao.UserDao;
 import com.harry.exception.ParameterException;
@@ -11,6 +13,7 @@ import com.harry.utils.StringUtil;
 
 public class UserServiceImpl implements UserService {
 
+    private Logger logger = Logger.getLogger(UserServiceImpl.class);
     private UserDao userDao;
 
     public void setUserDao(UserDao userDao) {
@@ -19,12 +22,15 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User login(String userName, String password) throws ParameterException {
+        logger.info("userName login");
         ParameterException parameterException = new ParameterException();
         if (StringUtil.isEmpty(userName)) {
             parameterException.putErrorMsg(Const.PARAM_USER_NAME, "User name is requied.");
+            logger.info("User name is requied.");
         }
         if (StringUtil.isEmpty(password)) {
             parameterException.putErrorMsg(Const.PARAM_PWD, "Password is requied.");
+            logger.info("Password is requied.");
         }
         if (parameterException.hasErrorMsg()) {
             throw parameterException;
