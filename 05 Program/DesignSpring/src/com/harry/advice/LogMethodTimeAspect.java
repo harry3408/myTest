2a07aad9ff39705bbefea1874d88e73ea73586ad
harryZou <963099010@qq.com>
2016-02-17 15:42:32 +0800

    Finsih the aspect.

--- 05 Program/DesignSpring/src/com/harry/advice/LogMethodTimeAspect.java
+++ 05 Program/DesignSpring/src/com/harry/advice/LogMethodTimeAspect.java
@@ -1,44 +1,44 @@
 package com.harry.advice;
 
-import org.aopalliance.intercept.MethodInvocation;
 import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 
 import com.harry.common.AppContext;
 import com.harry.common.Const;
-import com.harry.service.impl.LogMethodTime;
 
 public class LogMethodTimeAspect {
-	private final Logger logger = Logger.getLogger(LogMethodTimeAspect.class);
+    private final Logger logger = Logger.getLogger(LogMethodTimeAspect.class);
 
-	public void doBefore() {
-		logger.info("...dobefore...");
-	}
+    public void doBefore(JoinPoint jp) {
+        logger.info("...doBefore...");
+    }
 
-	public Object doAround(MethodInvocation invocation) throws Throwable {
-		System.out.println("Invoke method");
-		long startTime = System.currentTimeMillis();
+    public Object doAround(ProceedingJoinPoint pjp) throws Throwable {
+        System.out.println("Invoke method");
+        long startTime = System.currentTimeMillis();
 
-		Object returnVal = invocation.proceed();
+        Object returnVal = pjp.proceed();
 
-		long endTime = System.currentTimeMillis();
-		StringBuilder sb = new StringBuilder();
-		sb.append(AppContext.getAppContext().getObject(Const.APP_CONTEXT_USER_NAME));
-		sb.append(":");
-		sb.append(invocation.getMethod().getDeclaringClass().getSimpleName());
-		sb.append(":");
-		sb.append(invocation.getMethod().getName());
-		sb.append(":");
-		sb.append(startTime - endTime);
-		logger.info(sb.toString());
+        long endTime = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder();
+        sb.append(AppContext.getAppContext().getObject(Const.APP_CONTEXT_USER_NAME));
+        sb.append(":");
+        sb.append(pjp.getTarget().getClass().getSimpleName());
+        sb.append(":");
+        sb.append(pjp.getSignature().getName());
+        sb.append(":");
+        sb.append(startTime - endTime);
+        logger.info(sb.toString());
 
-		return returnVal;
-	}
+        return returnVal;
+    }
 
-	public void doAfter() {
-		logger.info("...doAfter...");
-	}
+    public void doAfter(JoinPoint jp) {
+        logger.info("...doAfter...");
+    }
 
-	public void afterThrowing() {
-		logger.info("...doThrowing...");
-	}
+    public void afterThrowing(JoinPoint jp) {
+        logger.info("...doThrowing...");
+    }
 }
