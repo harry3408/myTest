49b687f1f8a7c37c2857e6622e15b40073819b1f
harryZou <963099010@qq.com>
2016-02-18 19:50:54 +0800

    Spring-mvc

--- 05 Program/DesignSpring/src/com/harry/controller/UserController.java
+++ 05 Program/DesignSpring/src/com/harry/controller/UserController.java
@@ -5,16 +5,23 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
-import com.harry.common.AppContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
 import com.harry.common.Const;
-import com.harry.common.ModelAndView;
 import com.harry.exception.ParameterException;
 import com.harry.model.User;
 import com.harry.service.UserService;
 import com.harry.utils.StringUtil;
 
+@Controller
+@RequestMapping("/")
 public class UserController {
     private UserService userService;
 
@@ -22,39 +29,44 @@ public class UserController {
         this.userService = userService;
     }
 
-    public ModelAndView users(Map<String, Object> sessions, Map<String, String> paramters) throws IOException, ServletException {
+    @RequestMapping(value = "users.action", method = RequestMethod.GET)
+    public ModelAndView users(Map<String, Object> sessions, Map<String, String> paramters) throws IOException,
+            ServletException {
         ModelAndView modelAndView = new ModelAndView();
 
         List<User> users = userService.getAllUsers();
-        modelAndView.addRequestParameter(Const.PARAM_USER_LIST, users);
-        modelAndView.setView("sucess");
+        modelAndView.addObject(Const.PARAM_USER_LIST, users);
+        modelAndView.setViewName("list");
         return modelAndView;
     }
 
+    @RequestMapping(value = "delete.action", method = RequestMethod.GET)
     public ModelAndView delete(Map<String, Object> sessions, Map<String, String> paramters) throws IOException {
         ModelAndView modelAndView = new ModelAndView();
 
         String id = paramters.get("id");
         userService.delete(Integer.parseInt(id));
-        modelAndView.setView("sucess");
-        System.out.println("hello1");
+        modelAndView.setView(new RedirectView("users.action"));
         return modelAndView;
     }
 
-    public ModelAndView edit(Map<String, Object> sessions, Map<String, String> paramters) throws IOException, ServletException {
+    @RequestMapping(value = "edit.action", method = RequestMethod.GET)
+    public ModelAndView edit(Map<String, Object> sessions, Map<String, String> paramters) throws IOException,
+            ServletException {
         ModelAndView modelAndView = new ModelAndView();
 
         String userId = paramters.get("id");
         if (!StringUtil.isEmpty(userId)) {
             User user = userService.getUserById(Integer.parseInt(userId));
             if (user != null) {
-                modelAndView.addRequestParameter(Const.PARAM_USER, user);
+                modelAndView.addObject(Const.PARAM_USER, user);
             }
         }
-        modelAndView.setView("sucess");
+        modelAndView.setViewName("edit");
         return modelAndView;
     }
 
+    @RequestMapping(value = "save.action", method = RequestMethod.POST)
     public ModelAndView save(Map<String, Object> sessions, Map<String, String> paramters) throws IOException {
         ModelAndView modelAndView = new ModelAndView();
         User user = new User();
@@ -70,49 +82,50 @@ public class UserController {
         } else {
             userService.create(user);
         }
-        modelAndView.setView("sucess");
+        modelAndView.setView(new RedirectView("users.action"));
         return modelAndView;
     }
 
-    public ModelAndView login(Map<String, Object> sessions, Map<String, String> paramters) throws IOException, ServletException {
+    @RequestMapping(value = "login.action", method = RequestMethod.GET)
+    public ModelAndView login(Map<String, Object> sessions, Map<String, String> paramters) throws IOException,
+            ServletException {
         ModelAndView modelAndView = new ModelAndView();
 
-        AppContext appcontext = AppContext.getAppContext();
-        HttpServletRequest request = (HttpServletRequest) appcontext.getObject(Const.APP_CONTEXT_REQUEST);
-
         if (sessions.get(Const.PARAM_USER) != null) {
-            modelAndView.setView(Const.USERS_SELVLET);
+            modelAndView.setView(new RedirectView(Const.USERS_SELVLET));
         } else {
             String goPage = paramters.get("go");
             if (!StringUtil.isEmpty(goPage)) {
-                modelAndView.addRequestParameter("go", goPage);
+                modelAndView.addObject("go", goPage);
             }
-            modelAndView.setView("sucess");
-            modelAndView.setRedirect(false);
+            modelAndView.setViewName("login");
         }
         return modelAndView;
     }
 
-    public ModelAndView saveLogin(Map<String, Object> sessions, Map<String, String> paramters) throws IOException, ServletException {
+    @RequestMapping(value = "saveLogin.action", method = RequestMethod.POST)
+    public ModelAndView saveLogin(@RequestParam(value = "userName", defaultValue = "") String userName,
+            @RequestParam(value = "password", defaultValue = "") String password,
+            @RequestParam(value = "go", defaultValue = "") String go, HttpSession session) throws IOException,
+            ServletException {
         ModelAndView modelAndView = new ModelAndView();
 
         try {
-            User user = userService.login(paramters.get("userName"), paramters.get("password"));
+            User user = userService.login(userName, password);
             if (user == null) {
-                modelAndView.addRequestParameter(Const.PARAM_LOGIN_ERROR, "userName or password is wrong.");
-                modelAndView.setView("error");
+                modelAndView.addObject(Const.PARAM_LOGIN_ERROR, "userName or password is wrong.");
+                modelAndView.setViewName("login");
             } else {
-                modelAndView.addRequestSession(Const.PARAM_USER, user);
-                String goPage = paramters.get("go");
-                if (!StringUtil.isEmpty(goPage)) {
-                    modelAndView.setView(goPage);
+                session.setAttribute(Const.PARAM_USER, user);
+                if (!StringUtil.isEmpty(go)) {
+                    modelAndView.setView(new RedirectView(go));
                 } else {
-                    modelAndView.setView("sucess");
+                    modelAndView.setView(new RedirectView("users.action"));
                 }
             }
         } catch (ParameterException e) {
-            modelAndView.addRequestParameter(Const.PARAM_REQUIRED, e.getErrorMsg());
-            modelAndView.setView("error");
+            modelAndView.addObject(Const.PARAM_REQUIRED, e.getErrorMsg());
+            modelAndView.setViewName("login");
         }
         return modelAndView;
     }
